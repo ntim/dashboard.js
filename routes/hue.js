bcff49328f8ac1e5a14b837d845ce618b63d09c3
Tim Niggemann <niggemann@physik.rwth-aachend.de>
2014-08-20 23:03:56 +0200

    Philips hue buttons.

--- routes/hue.js
+++ routes/hue.js
@@ -1,33 +1,87 @@
 var express = require('express');
 var router = express.Router();
 var hue = require("node-hue-api");
+var request = require("request");
+var util = require('util');
+
+var username = "newdeveloper";
+var host = null;
+var api = null;
+
+function discover() {
+	hue.locateBridges().then(function(bridges) {
+		host = bridges[0].ipaddress;
+		api = new hue.HueApi(host, username);
+	}).done();
+}
 
 /* GET off page. */
-router.get('/bridge', function(req, res) {
+router.get('/bridges', function(req, res) {
 	hue.locateBridges().then(function(bridge) {
 		res.json(bridge);
 	}).done();
 });
 
+router.get('/lights', function(req, res) {
+	var path = util.format("http://%s/api/%s/lights", host, username);
+	request(path, function(error, response, lights) {
+		if (error) {
+			return res.json(error);
+		}
+		res.json(JSON.parse(lights));
+	});
+});
+
 /* GET off page. */
 router.get('/off', function(req, res) {
 	var state = hue.lightState.create().off();
-	hue.locateBridges().then(function(bridge) {
-		var api = new HueApi(bridge.ipaddress, "newdeveloper");
-		api.lights(function(err, lights) {
-			if (err) {
-				console.log(err);
-				return;
-			}
-			lights.forEach(function(l) {
-				api.setLightState(l.id, state, function(err, lights) {
-					if (err) {
-						console.log(err);
-					}
-				});
+	api.lights(function(error, result) {
+		var lights = result.lights;
+		// Do the setting of the light sync since the bridge only accepts a few
+		// concurrent connections.
+		function set(i) {
+			api.setLightState(lights[i].id, state, function(error, result) {
+				if (error) {
+					return res.json(error);
+				}
+				if (i + 1 < lights.length) {
+					set(i + 1);
+				} else {
+					res.json({
+						success : "off"
+					});
+				}
 			});
-		});
-	}).done();
+		}
+		set(0);
+	});
 });
+
+/* GET off page. */
+router.get('/on', function(req, res) {
+	var state = hue.lightState.create().on();
+	api.lights(function(error, result) {
+		var lights = result.lights;
+		// Do the setting of the light sync since the bridge only accepts a few
+		// concurrent connections.
+		function set(i) {
+			api.setLightState(lights[i].id, state, function(error, result) {
+				if (error) {
+					return res.json(error);
+				}
+				if (i + 1 < lights.length) {
+					set(i + 1);
+				} else {
+					res.json({
+						success : "off"
+					});
+				}
+			});
+		}
+		set(0);
+	});
+});
+
+discover();
 
 module.exports = router;
